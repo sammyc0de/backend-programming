2707ac6b1895a07d701dab5d07fb19acbf099daf
Sami Hiltunen <sami@Samis-Air.localdomain>
2026-09-18 22:21:52 +0300

    Bookstore REST service updated

--- bookstore/src/main/java/syksy26/bookstore/web/BookRestController.java
+++ bookstore/src/main/java/syksy26/bookstore/web/BookRestController.java
@@ -2,8 +2,12 @@ package syksy26.bookstore.web;
 
 import java.util.Optional;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import syksy26.bookstore.domain.Book;
@@ -28,6 +32,24 @@ public class BookRestController {
     public Optional<Book> findById(@PathVariable("id") Long bookId) {
         return bookRepository.findById(bookId);
     }
+
+    @PostMapping("/books")
+    public Book saveBook(@RequestBody Book book) {		
+        return bookRepository.save(book);
+    }
+
+    @PutMapping("books/{id}")
+    public Book saveEditedBook(@RequestBody Book editedBook, @PathVariable Long id) {
+		editedBook.setId(id);
+		return bookRepository.save(editedBook);
+	}
+    @DeleteMapping("/books/{id}")   
+    public Iterable<Book> deleteBook(@PathVariable Long id) {
+        
+        bookRepository.deleteById(id);
+        return bookRepository.findAll();
+    }
+
 
 
 }
